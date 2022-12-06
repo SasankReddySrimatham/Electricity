145fcd43d1a4803328e66ccdba34366d290bec47
SRIMATHAM <sasank-reddy.srimatham@capgemini.com>
2022-12-06 14:50:30 +0530

    first commit

---
+++ src/main/java/com/electricity_demo/ElectricityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.electricity_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityDemoApplication.class, args);
+	}
+
+}
